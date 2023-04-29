70bf30cf760315e8dc097f47787b09b5e97a4458
Elucid <paulujlaky@gmail.com>
2023-04-29 18:27:56 -0400

    Add word bank (initial)

--- scripts/crossword.js
+++ scripts/crossword.js
@@ -1,6 +1,49 @@
+const WordsAndDefinitions = [
+
+    ["Limit", "The value that a function approaches as the input approaches a specific value or infinity."],
+    ["Continuity", "A property of a function where it is continuous, meaning it has no breaks, gaps, or holes in its graph."],
+    ["Derivative", "A measure of how a function changes as its input changes, representing the slope of the tangent line to the function's graph."],
+    ["Integral", "A fundamental concept in calculus that represents the area under a curve or the accumulation of a quantity over an interval."],
+    ["ChainRule", "A rule used to differentiate a composite function by taking the derivative of the outer function and multiplying it by the derivative of the inner function."],
+    ["ProductRule", "A rule used to differentiate the product of two functions by taking the derivative of each function and adding their products."],
+    ["QuotientRule", "A rule used to differentiate the quotient of two functions by taking the derivative of each function and subtracting their products, divided by the square of the denominator function."],
+    ["Inflection", "A point on a curve where the function changes concavity or the second derivative changes sign."],
+    ["Asymptote", "A line that a function approaches but never touches as the input approaches infinity or negative infinity."],
+    ["Antiderivative", "A function whose derivative is equal to a given function."],
+    ["Convergence", "A property of a series or sequence where it approaches a finite limit as the number of terms increases."],
+    ["Divergence", "A property of a series or sequence where it does not approach a finite limit as the number of terms increases."],
+    ["Substitution", "A method used to evaluate integrals by replacing the variable with a new variable, simplifying the integral."],
+    ["Implicit", "A way of representing a function or equation without explicitly solving for one variable in terms of the other."],
+    ["Extrema", "The maximum and minimum values of a function over a specified interval."],
+    ["Rolle's", "A theorem stating that if a function is continuous on a closed interval and differentiable on the open interval, and has equal values at the endpoints, then there exists at least one point in the open interval where the derivative is zero."],
+    ["L'Hopital's", "A rule used to evaluate the limit of a quotient of two functions when the limit of the numerator and denominator is either zero or infinity, by taking the limit of the quotient of their derivatives."],
+    ["Tangent", "A straight line that touches a curve at a single point and has the same slope as the curve at that point."],
+    ["Normal", "A straight line that is perpendicular to the tangent line at the point of tangency on a curve."],
+    ["Secant", "A straight line that intersects a curve at two or more points."],
+    ["Cotangent", "The reciprocal of the tangent function in trigonometry, equal to the ratio of the adjacent side to the opposite side in a right triangle" ],
+    ["Cosecant", "The reciprocal of the sine function in trigonometry, equal to the ratio of the hypotenuse to the opposite side in a right triangle."],
+    ["Exponential", "A function in which the variable is an exponent, and the base is a constant."],
+    ["Logarithmic", "The inverse function of an exponential function, representing the power to which the base must be raised to produce a given value."],
+    ["Hyperbolic", "A set of functions that are analogs of trigonometric functions but are based on hyperbolas instead of circles."],
+    ["Domain", "The set of all possible input values for a function."],
+    ["Range", "The set of all possible output values for a function."],
+    ["Endpoint", "A point at the beginning or end of a line segment or interval."],
+    ["Slope", "A measure of the steepness of a line, equal to the ratio of the vertical change to the horizontal change between any two points on the line."],
+    ["Amplitude", "The maximum displacement of a periodic function from its mean value."],
+    ["Frequency", "The number of complete cycles or oscillations of a periodic function in a unit of time or space."],
+    ["Midline", "A horizontal line that represents the average value of a periodic function over one period."],
+    ["Discontinuity", "A point at which a function is not continuous, meaning there is a break, gap, or hole in its graph."],
+    ["Optimization", "The process of finding the maximum or minimum value of a function, often used to solve real-world problems."],
+    ["Riemann", "A method for approximating the definite integral of a function by summing the areas of rectangles or trapezoids over an interval."],
+    ["Trapezoidal", "A rule for approximating the definite integral of a function by summing the areas of trapezoids over an interval."],
+    ["Absolute", "The non-negative value of a number, representing its distance from zero on the number line."],
+
+]
+
 function Generate() { 
 
 
 
     
 }
+
