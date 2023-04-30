d3d08006a20ebe417e6b8e26668386b41250e75a
Elucid <paulujlaky@gmail.com>
2023-04-29 21:36:50 -0400

    Looks the part

--- scripts/crossword.js
+++ scripts/crossword.js
@@ -12,10 +12,10 @@ const WordsAndDefinitions = [
     ["Antiderivative", "A function whose derivative is equal to a given function."],
     ["Convergence", "A property of a series or sequence where it approaches a finite limit as the number of terms increases."],
     ["Divergence", "A property of a series or sequence where it does not approach a finite limit as the number of terms increases."],
-    ["Substitution", "A method used to evaluate integrals by replacing the variable with a new variable, simplifying the integral."],
+    ["Substitution", "A method used to evaluate integrals by replacing the letiable with a new letiable, simplifying the integral."],
     ["Extrema", "The maximum and minimum values of a function over a specified interval."],
     ["Rolle's Theorem", "A theorem stating that if a function is continuous on a closed interval and differentiable on the open interval, and has equal values at the endpoints, then there exists at least one point in the open interval where the derivative is zero."],
-    ["L'Hopital's", "A rule used to evaluate the limit of a quotient of two functions when the limit of the numerator and denominator is either zero or infinity, by taking the limit of the quotient of their derivatives."],
+    ["L'Hopital's Rule", "A rule used to evaluate the limit of a quotient of two functions when the limit of the numerator and denominator is either zero or infinity, by taking the limit of the quotient of their derivatives."],
     ["Tangent Line", "A straight line that touches a curve at a single point and has the same slope as the curve at that point."],
     ["Normal Line", "A straight line that is perpendicular to the tangent line at the point of tangency on a curve."],
     ["Secant Line", "A straight line that intersects a curve at two or more points."],
@@ -34,173 +34,253 @@ const WordsAndDefinitions = [
     
 ]
 
+function getRandomElements(arr, count) {
+
+  const shuffled = [...arr];
+  for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled.slice(0, count);
+
+}
+
 function Generate() { 
 
-    var board, wordArr, wordBank, wordsActive, mode;
+    let board, wordArr, wordBank, wordsActive, mode;
 
-    var Bounds = {  
-      top:0, right:0, bottom:0, left:0,
+    const Bounds = {  
+      
+      top:500, right:0, bottom:0, left:500,
     
-      Update:function(x,y){
+      Update: function (x, y) {
+        
         this.top = Math.min(y,this.top);
         this.right = Math.max(x,this.right);
         this.bottom = Math.max(y,this.bottom);
-        this.left = Math.min(x,this.left);
+        this.left = Math.min(x, this.left);
+        
       },
       
-      Clean:function(){
-        this.top = 999;
+      Clean: function () {
+        
+        this.top = 500;
         this.right = 0;
         this.bottom = 0;    
-        this.left = 999;
+        this.left = 500;
+
       }
-    };
+
+    }
     
-    
-    //---------------------------------//
-    //   MAIN                          //
-    //---------------------------------//
-    
-    function Play(){
-      var letterArr = document.getElementsByClassName('letter');
+    function Play() {
       
-      for(var i = 0; i < letterArr.length; i++){
+      let letterArr = document.getElementsByClassName('letter');
+      
+      for (let i = 0; i < letterArr.length; i++){
+        
         letterArr[i].innerHTML = "<input class='char' type='text' maxlength='1'></input>";
+
       }
       
       mode = 0;
       ToggleInputBoxes(false);
+
     }
     
-    
     function Create(){
-      if (mode === 0){
+      
+      if (mode === 0) {
+        
         ToggleInputBoxes(true);
         document.getElementById("crossword").innerHTML = BoardToHtml(" ")
         mode = 1;
+
       }
-      else{  
-        GetWordsFromInput();
+
+      else {  
+        
+        AssignWords();
+        
+        let isSuccess = true
     
-        for(var i = 0, isSuccess=false; i < 10 && !isSuccess; i++){
-          CleanVars();
+        for (let i = 0, isSuccess = false; i < 10 && !isSuccess; i++){
+          
+          Cleanlets();
+
           isSuccess = PopulateBoard();
+
         }
     
-        document.getElementById("crossword").innerHTML = 
-          (isSuccess) ? BoardToHtml(" ") : "Failed to find crossword." ;
+        document.getElementById("crossword").innerHTML = (isSuccess) ? BoardToHtml(" ") : "Failed to find crossword.";
+        
       }
     }
     
     
     function ToggleInputBoxes(active){
-      var w=document.getElementsByClassName('word'),
-          d=document.getElementsByClassName('clue');
       
-      for(var i=0;i<w.length; i++){
-        if(active===true){
+      let w= document.getElementsByClassName('word')
+      let d = document.getElementsByClassName('clue')
+      
+      for (let i = 0; i < w.length; i++){
+        
+        if (active === true) {
+          
           RemoveClass(w[i], 'hide');
           RemoveClass(d[i], 'clueReadOnly');
           d[i].disabled = '';
+
         }
-        else{
+
+        else {
+          
           AddClass(w[i], 'hide');
           AddClass(d[i], 'clueReadOnly');
           d[i].disabled = 'readonly';
+
         }
       }
     }
     
-    
-    function GetWordsFromInput(){
+    function AssignWords(){
       
         wordArr = [];  
         let index = 0
+        
+        // Pull 6 random elements from the WordsAndDefinitions array
+      
+        let elements = getRandomElements(WordsAndDefinitions, 7);
+        
+        console.log(elements.map(([word, definition]) => word))
     
-        for (const [word, definition] of WordsAndDefinitions) {
-            if (index < 5) wordArr.push(word);
+        for (const [word, definition] of elements) {
+            if (index < 7) wordArr.push(word);
             index += 1;
         }
 
-
     }
     
     
-    function CleanVars(){
-      Bounds.Clean();
-      wordBank = [];
-      wordsActive = [];
-      board = [];
+  function Cleanlets() {
       
-      for(var i = 0; i < 32; i++){
+    Bounds.Clean();
+    
+    wordBank = [];
+    
+    wordsActive = [];
+    
+    board = [];
+    
+    for (let i = 0; i < 32; i++){
+        
         board.push([]);
-        for(var j = 0; j < 32; j++){
+        for(let j = 0; j < 32; j++){
           board[i].push(null);
-        }
       }
-    }
-    
-    
-    function PopulateBoard(){
-      PrepareBoard();
       
-      for(var i=0,isOk=true,len=wordBank.length; i<len && isOk; i++){
-        isOk = AddWordToBoard();
-      }  
-      return isOk;
     }
     
+  }
+
+  function PopulateBoard() {
+      
+      PrepareBoard();
+
+      let isOk = true;
+      
+      for (let i = 0, isOk = true, len = wordBank.length; i < len && isOk; i++){
+        
+        isOk = AddWordToBoard();
+
+    }  
     
-    function PrepareBoard(){
+    return isOk;
+    
+  }
+
+  function PrepareBoard() {
+      
       wordBank=[];
       
-      for(var i = 0, len = wordArr.length; i < len; i++){
-        wordBank.push(new WordObj(wordArr[i]));
-      }
+    for (let i = 0, len = wordArr.length; i < len; i++){
+        
+      wordBank.push(new WordObj(wordArr[i]));
       
-      for(i = 0; i < wordBank.length; i++){
-        for(var j = 0, wA=wordBank[i]; j<wA.char.length; j++){
-          for(var k = 0, cA=wA.char[j]; k<wordBank.length; k++){
-            for(var l = 0,wB=wordBank[k]; k!==i && l<wB.char.length; l++){
-              wA.totalMatches += (cA === wB.char[l])?1:0;
-            }
-          }
-        }
-      }  
     }
     
+      
+    for (i = 0; i < wordBank.length; i++){
+        
+      for (let j = 0, wA = wordBank[i]; j < wA.char.length; j++){
+          
+        for (let k = 0, cA = wA.char[j]; k < wordBank.length; k++){
+            
+          for (let l = 0, wB = wordBank[k]; k !== i && l < wB.char.length; l++){
+              
+            wA.totalMatches += (cA === wB.char[l]) ? 1 : 0;
+            
+            }
+        }
+        
+      }
+      
+    }  
     
-    // TODO: Clean this guy up
-    function AddWordToBoard(){
-      var i, len, curIndex, curWord, curChar, curMatch, testWord, testChar, 
-          minMatchDiff = 9999, curMatchDiff;  
+  }
+  
+  function AddWordToBoard() {
+
+    let i, len, curIndex, curWord, curChar, testWord, testChar, 
+        
+      minMatchDiff = 9999, curMatchDiff;  
     
-      if(wordsActive.length < 1){
-        curIndex = 0;
-        for(i = 0, len = wordBank.length; i < len; i++){
+    if (wordsActive.length < 1) {
+        
+      curIndex = 0;
+      
+      for (i = 0, len = wordBank.length; i < len; i++){
+          
           if (wordBank[i].totalMatches < wordBank[curIndex].totalMatches){
             curIndex = i;
-          }
+
         }
-        wordBank[curIndex].successfulMatches = [{x:12,y:12,dir:0}];
-      }
-      else{  
-        curIndex = -1;
         
-        for(i = 0, len = wordBank.length; i < len; i++){
-          curWord = wordBank[i];
-          curWord.effectiveMatches = 0;
-          curWord.successfulMatches = [];
-          for(var j = 0, lenJ = curWord.char.length; j < lenJ; j++){
-            curChar = curWord.char[j];
-            for (var k = 0, lenK = wordsActive.length; k < lenK; k++){
-              testWord = wordsActive[k];
-              for (var l = 0, lenL = testWord.char.length; l < lenL; l++){
-                testChar = testWord.char[l];            
-                if (curChar === testChar){
-                  curWord.effectiveMatches++;
+      }
+      
+      wordBank[curIndex].successfulMatches = [{ x: 12, y: 12, dir: 0 }];
+      
+    }
+    
+    else {  
+      
+      curIndex = -1;
+      
+      for (i = 0, len = wordBank.length; i < len; i++){
+          
+        curWord = wordBank[i];
+        
+        curWord.effectiveMatches = 0;
+        
+        curWord.successfulMatches = [];
+        
+        for (let j = 0, lenJ = curWord.char.length; j < lenJ; j++){
+            
+          curChar = curWord.char[j];
+          
+          for (let k = 0, lenK = wordsActive.length; k < lenK; k++){
+              
+            testWord = wordsActive[k];
+            
+            for (let l = 0, lenL = testWord.char.length; l < lenL; l++){
+                
+              testChar = testWord.char[l];            
+              
+              if (curChar === testChar) {
                   
-                  var curCross = {x:testWord.x,y:testWord.y,dir:0};              
+                curWord.effectiveMatches++;
+                
+                  let curCross = {x:testWord.x,y:testWord.y,dir:0};              
                   if(testWord.dir === 0){                
                     curCross.dir = 1;
                     curCross.x += l;
@@ -212,25 +292,36 @@ function Generate() {
                     curCross.x -= j;
                   }
                   
-                  var isMatch = true;
+                  let isMatch = true;
                   
-                  for(var m = -1, lenM = curWord.char.length + 1; m < lenM; m++){
-                    var crossVal = [];
+                  for(let m = -1, lenM = curWord.char.length + 1; m < lenM; m++){
+                    let crossVal = [];
                     if (m !== j){
                       if (curCross.dir === 0){
-                        var xIndex = curCross.x + m;
+                        let xIndex = curCross.x + m;
                         
                         if (xIndex < 0 || xIndex > board.length){
                           isMatch = false;
                           break;
                         }
+
+                        if (board[xIndex]) {
                                                   
-                        crossVal.push(board[xIndex][curCross.y]);
-                        crossVal.push(board[xIndex][curCross.y + 1]);
-                        crossVal.push(board[xIndex][curCross.y - 1]);
+                          crossVal.push(board[xIndex][curCross.y]);
+                          crossVal.push(board[xIndex][curCross.y + 1]);
+                          crossVal.push(board[xIndex][curCross.y - 1]);
+                          
+                        }
+
+                        else { 
+
+                          console.warn("Out of bounds")
+
+                        }
+
                       }
                       else{
-                        var yIndex = curCross.y + m;
+                        let yIndex = curCross.y + m;
                         
                         if (yIndex < 0 || yIndex > board[curCross.x].length){
                           isMatch = false;
@@ -289,10 +380,10 @@ function Generate() {
         return false;
       }
         
-      var spliced = wordBank.splice(curIndex, 1);
+      let spliced = wordBank.splice(curIndex, 1);
       wordsActive.push(spliced[0]);
       
-      var pushIndex = wordsActive.length - 1,
+      let pushIndex = wordsActive.length - 1,
           rand = Math.random(),
           matchArr = wordsActive[pushIndex].successfulMatches,
           matchIndex = Math.floor(rand * matchArr.length),  
@@ -303,7 +394,7 @@ function Generate() {
       wordsActive[pushIndex].dir = matchData.dir;
       
       for(i = 0, len = wordsActive[pushIndex].char.length; i < len; i++){
-        var xIndex = matchData.x,
+        let xIndex = matchData.x,
             yIndex = matchData.y;
         
         if (matchData.dir === 0){
@@ -322,10 +413,13 @@ function Generate() {
     }
     
     
-    function BoardToHtml(blank){
-      for(var i=Bounds.top-1, str=""; i<Bounds.bottom+2; i++){
-        str+="<div class='row'>";
-        for(var j=Bounds.left-1; j<Bounds.right+2; j++){
+  function BoardToHtml(blank) {
+      
+    let str = "";
+    
+      for (var i = Bounds.top - 1; i<Bounds.bottom+2; i++){
+        str += "<div class='row'>";
+        for (let j = Bounds.left - 1; j < Bounds.right + 2; j++){
           str += BoardCharToElement(board[j][i]);
         }
         str += "</div>";
@@ -333,17 +427,10 @@ function Generate() {
       return str;
     }
     
-    
     function BoardCharToElement(c){
-      var arr=(c)?['square','letter']:['square'];
+      let arr=(c)?['square','letter']:['square'];
       return EleStr('div',[{a:'class',v:arr}],c);
     }
-    
-    
-    
-    //---------------------------------//
-    //   OBJECT DEFINITIONS            //
-    //---------------------------------//
     
     function WordObj(stringValue){
       this.string = stringValue;
@@ -353,22 +440,13 @@ function Generate() {
       this.successfulMatches = [];  
     }
     
-    
-    //---------------------------------//
-    //   EVENTS                        //
-    //---------------------------------//
-    
     function RegisterEvents(){
       document.getElementById("crossword").onfocus = function (){ 
         return false; }
     }
     RegisterEvents();
     
-    
-    //---------------------------------//
-    //   HELPER FUNCTIONS              //
-    //---------------------------------//
-    
+
     function EleStr(e,c,h){
       h = (h)?h:"";
       for(var i=0,s="<"+e+" "; i<c.length; i++){
@@ -391,21 +469,12 @@ function Generate() {
     function RemoveClass(ele,classStr){
       ele.className = ele.className.replaceAll(' '+classStr,'');
     }
-    
-    function ToggleClass(ele,classStr){
-      var str = ele.className.replaceAll(' '+classStr,'');
-      ele.className = (str.length===ele.className.length)?str+' '+classStr:str;
-    }
-    
+      
     String.prototype.replaceAll = function (replaceThis, withThis) {
-       var re = new RegExp(replaceThis,"g"); 
+       let re = new RegExp(replaceThis,"g"); 
        return this.replace(re, withThis);
     };
     
-    
-    //---------------------------------//
-    //   INITIAL LOAD                  //
-    //---------------------------------//
     
     Create();
     ToggleInputBoxes()
